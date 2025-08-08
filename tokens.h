0bca5f5b4143e6d651c00905d0e8fc5326941708
Marc SIBERT <Marcussacapuces91@users.noreply.github.com>
2025-08-08 09:27:40 +0200

    Update tokens.h
    string_view

--- tokens.h
+++ tokens.h
@@ -20,6 +20,11 @@
 #include <string>
 #include <memory>
 
+#ifndef LOG_LOCAL_LEVEL
+#define LOG_LOCAL_LEVEL ESP_LOG_INFO
+#endif
+#include <esp_log.h>
+
 #pragma once
 
 class TokenSpaces;
@@ -28,20 +33,25 @@ class TokenInstruction;
 
 class Token {
 public:
-  Token(const std::string& aToken): token(aToken) {};
+  Token(const std::string_view aToken): token(aToken) {};
   virtual ~Token() = default;
 
-  virtual std::string to_string() const = 0;
+  virtual std::string to_string() const {
+    return token;
+  }
 
-  static std::unique_ptr<const TokenSpaces> parseSpaces(const std::string& line, size_t& end);
-  static std::unique_ptr<const Token> parseNum(const std::string& line, size_t& end);
-  static std::unique_ptr<const TokenString> parseString(const std::string& line, size_t& end);
-  static std::unique_ptr<const TokenInstruction> parseInstruction(const std::string& line, size_t& end);
+  static std::unique_ptr<const TokenSpaces> parseSpaces(const std::string_view line, size_t& end);
+  static std::unique_ptr<const Token> parseNum(const std::string_view line, size_t& end);
+  static std::unique_ptr<const TokenString> parseString(const std::string_view line, size_t& end);
+  static std::unique_ptr<const TokenInstruction> parseInstruction(const std::string_view line, size_t& end);
+  static std::unique_ptr<const Token> parseSeparator(const std::string_view line, size_t& end);
 
 protected:
   const std::string token;
 
-  static long int parseInt(const std::string& line, size_t& end) {
+  static long int parseInt(const std::string_view line, size_t& end) {
+    if (!line.size()) { end = 0; return 0; }
+
     int res = 0;
     bool neg = false;
     int pos = 0;
@@ -63,21 +73,13 @@ protected:
 
 class TokenSpaces : public Token {
 public:
-  TokenSpaces(const std::string& aToken) : Token(aToken) {};
-
-  std::string to_string() const override {
-    return std::string{"<TOKEN_SPACES> "} + token;
-  }
+  TokenSpaces(const std::string_view aToken) : Token(aToken) {};
 
 };
 
 class TokenInteger : public Token {
 public:
-  TokenInteger(const std::string& aToken, const int aValue) : Token(aToken), value(aValue) {};
-
-  std::string to_string() const override {
-    return std::string{"<TOKEN_INTEGER> "} + token;
-  }
+  TokenInteger(const std::string_view aToken, const int aValue) : Token(aToken), value(aValue) {};
 
 private:
   const int value;
@@ -86,11 +88,7 @@ private:
 
 class TokenCommand : public Token {
 public:
-  TokenCommand(const std::string& aToken, const int aCmd) : Token(aToken), cmd(aCmd) {};
-
-  std::string to_string() const override {
-    return std::string{"<TOKEN_COMMAND> "} + token;
-  }
+  TokenCommand(const std::string_view aToken, const int aCmd) : Token(aToken), cmd(aCmd) {};
 
 private:
   const int cmd;
@@ -98,22 +96,24 @@ private:
 
 class TokenString : public Token {
 public:
-  TokenString(const std::string& aToken) : Token(aToken) {};
+  TokenString(const std::string_view aToken) : Token(aToken) {};
 
   std::string to_string() const override {
-    return std::string{"<TOKEN_STRING> "} + '"' + token + '"';
+    return '"' + token + '"';
   }
 
 };
 
 class TokenInstruction : public Token {
 public:
-  TokenInstruction(const std::string& aToken, const unsigned aInstruction) : Token(aToken), instruction(aInstruction) {};
-
-  std::string to_string() const override {
-    return std::string{"<TOKEN_INSTRUCTION> "} + token;
-  }
+  TokenInstruction(const std::string_view aToken, const unsigned aInstruction) : Token(aToken), instruction(aInstruction) {};
 
 private:
   const unsigned instruction;
+};
+
+class TokenSeparator : public Token {
+public:
+  TokenSeparator(const char aToken) : Token(std::string{1, aToken}) {};
+
 };
